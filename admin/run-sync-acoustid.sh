b44177769fdf3340942f96f84dd10913f330abad
Davis Devasia <davis143devasia@gmail.com>
2019-03-29 13:53:15 +0530

    Trying to get scripts working

--- admin/run-sync-acoustid.sh
+++ admin/run-sync-acoustid.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 DIR=`dirname $0`/..
+
 test -d $DIR/e && source $DIR/e/bin/activate
 export PYTHONPATH=$DIR:$PYTHONPATH
-${DIR}/scripts/acoustid_sync.py -c ${DIR}/acoustid.conf
+
+python ${DIR}/scripts/acoustid_sync.py -c ${DIR}/acoustid.conf
 exit $?
 
